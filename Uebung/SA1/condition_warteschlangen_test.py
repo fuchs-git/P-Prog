ec5f0b1f1808d6ef32851657ace8769b294a0108
fuchs <pasvoss@gmail.com>
2025-11-10 08:49:16 +0100

    Uebung Drohne - Warteschlange

--- Uebung/SA1/condition_warteschlangen_test.py
+++ Uebung/SA1/condition_warteschlangen_test.py
@@ -1,19 +1,28 @@
 from threading import Thread, Condition
 from time import sleep
 
-
 def test(i: int):
-    with condition:
-        if i == 0:
-            sleep(3)
-        liste_danach.append(i)
-        # Nächster darf rein
-        condition.notify_all()
+    with bedingung:
+        # warten, bis ich an der Reihe bin
+        warteschlange.append(i)
+        while i != warteschlange[0]:
+            bedingung.wait()
+
+    # kritischer Abschnitt
+    if i == 0:
+        sleep(3)
+    liste_danach.append(i)
+
+    # Nächster darf rein
+    with bedingung:
+        warteschlange.pop(0)
+        bedingung.notify_all()
 
 
-condition = Condition()
+bedingung = Condition()
 liste_davor = list(range(1000))
 liste_danach = []
+warteschlange = []  # mutable, damit per Referenz geändert werden kann
 
 threads = []
 for i in liste_davor:
@@ -21,11 +30,27 @@ for i in liste_davor:
 
 for thread in threads:
     thread.start()
-    sleep(0.01)
 
 for thread in threads:
     thread.join()
 
 print(liste_davor == liste_danach)
-print(liste_davor[:20])   # zur Übersicht
+print(liste_davor[:20])  # zur Übersicht
 print(liste_danach[:20])
+
+# Parkhaus
+def einfahren(self, auto: int):
+    print(f'\r[+] Das Parkhaus hat {self.anzahl_freie_plaetze} freie Plätze', end='', flush=True)
+
+    with self.condition:
+        self.wartende_autos.append(auto)
+        while self.wartende_autos[0] is not auto or self.anzahl_freie_plaetze <= 0:
+            self.condition.wait()
+
+        self.wartende_autos.pop(0)
+        self.anzahl_freie_plaetze -= 1
+
+def ausfahren(self):
+    with self.condition:
+        self.anzahl_freie_plaetze += 1
+        self.condition.notify_all()
